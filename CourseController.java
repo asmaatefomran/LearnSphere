b655408b04c55331e5e4c95cb26b574116723204
Nada936 <113146240+Nada936@users.noreply.github.com>
2024-12-19 22:10:56 +0200

    Add files via upload

---
+++ CourseController.java
@@ -0,0 +1,70 @@
+package com.example.LMS.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LMS.model.Course;
+import com.example.LMS.service.CourseService;
+
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    // Endpoint for creating a course
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.createCourse(
+                course.getTitle(),
+                course.getDescription(),
+                course.getInstructorId()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCourse);
+    }
+    // Endpoint to get course by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        Optional<Course> course = courseService.getCourseById(id);
+        return course.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Endpoint to retrieve all courses
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        boolean isDeleted = courseService.deleteCourse(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    @PutMapping("/{id}")
+     public ResponseEntity<Course> updateCourse(@PathVariable Long id,@RequestBody Course updatedCourse) {
+    boolean isUpdated = courseService.updateCourse(id, updatedCourse);
+
+    if (isUpdated) {
+        return ResponseEntity.ok(updatedCourse); 
+    } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); 
+    }
+}
+}
