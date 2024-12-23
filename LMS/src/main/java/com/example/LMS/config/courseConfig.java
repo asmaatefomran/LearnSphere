41a4edaae711a6d263a3622b44ec48267f9ede16
salma <salma.a.samei@gmail.com>
2024-12-23 23:37:02 +0200

    update

--- LMS/src/main/java/com/example/LMS/config/courseConfig.java
+++ LMS/src/main/java/com/example/LMS/config/courseConfig.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class courseConfig {
-    @Bean(name = "courseCommandLineRunner")
+public class CourseConfig {
+    @Bean(name = "CourseCommandLineRunner")
     CommandLineRunner commandLineRunner(CourseService repository) {
         return args -> {
-            repository.createCourse("CS","Course for first grade",6L);
-            repository.createCourse("DB"," Course for Second grade",6L);
-            repository.createCourse("Algo"," Course for Second grade",6L);
-            repository.createCourse("Arch"," Course for Third grade",6L);
+            repository.createCourse("CS","Course for first grade","6");
+            repository.createCourse("DB"," Course for Second grade","6");
+            repository.createCourse("Algo"," Course for Second grade","6");
+            repository.createCourse("Arch"," Course for Third grade","6");
         };
     }
 
